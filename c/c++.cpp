f5aef50c222aefe9c2f098c61d28e503333c18a8
varun-coder <44544783+varun-coder@users.noreply.github.com>
2018-10-28 16:37:44 +0530

    ok

--- c/c++.cpp
+++ c/c++.cpp
@@ -1,7 +1,6 @@
 #include <iostream.h> // include API
 
-int main() // the main code portion of a C++ program
+void main() // the main code portion of a C++ program
 {
-   cout << "Hello World" << endl;  //print Hello World on the screen 
-   return 0; // conventional
+   cout <<"Hello World";  //print Hello World on the screen 
 }
